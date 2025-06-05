1c3a4c4663481fbbef039c09b715168f8d294885
ad6ms <StansfieldA96@gmail.com>
2025-06-05 10:53:02 +0100

    Added first API endpoint for user registration. Installed nodemailer and added email authentication

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,13 +1,16 @@
 const bcrypt = require("bcrypt");
 const prisma = require("../../prismaClient");
 
+const generateAuthToken = require("../utils/generateAuthToken");
+const sendAuthEmail = require("../utils/sendAuthEmail");
+
 const registerUser = async (request, response) => {
   const { email, password, name } = request.body;
 
-  if (!email || !password) {
+  if (!email || !password || !name) {
     return response
       .status(400)
-      .json({ message: "Email and password are required" });
+      .json({ message: "Email, password, and name are required" });
   }
 
   try {
@@ -18,21 +21,36 @@ const registerUser = async (request, response) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const verificationToken = generateAuthToken();
+
     const user = await prisma.user.create({
       data: {
         email,
         password: hashedPassword,
         name,
+        role: "USER",
+        verified: false,
+        verificationToken,
       },
     });
+
+    await sendAuthEmail({
+      to: email,
+      subject: "Verify your email for Ticket",
+      text: `Your verification code is: ${verificationToken}`,
+      html: `<p> Your verification code is ${verificationToken} </p>`,
+    });
+
+    const { password: _, ...userWithoutPassword } = user;
+    response.status(201).json(userWithoutPassword);
   } catch (error) {
     console.error("error");
     response
       .status(500)
       .json({ message: "Server error, please try again later" });
   }
+};
 
-  module.exports = {
-    registerUser,
-  };
+module.exports = {
+  registerUser,
 };
---
+++ backend/src/utils/sendAuthEmail.js
@@ -0,0 +1,28 @@
+const nodemailer = require("nodemailer");
+
+const sendAuthEmail = async ({ to, subject, text, html }) => {
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      password: process.env.EMAIL_PASS,
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: ", info.response);
+  } catch (error) {
+    console.error("Error sending email: ", error);
+  }
+};
+
+module.exports = sendAuthEmail;
